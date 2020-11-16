a7448faf6aa49153b46af58cca8def94e1c5418a
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2020-11-16 17:41:33 +0530

    NA

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <p data-ns-test="project-name">hi this si project</p>
+      <p data-ns-test="project-description">and this is description</p>
+    </div>
+  );
+};
 
 export default App;
